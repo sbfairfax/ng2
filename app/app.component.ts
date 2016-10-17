c8512b24147c3c8625124ae874cd6e898bbe9366
Orlando.Lagman <orlando.lagman@quadramed.com>
2016-10-17 15:56:37 -0400

    add two-way binding to <input. element using ngModel directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-
 export class Hero {
   id: number;
   name: string;
 }
-
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
-
-
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
@@ -17,6 +21,4 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
-
 }
-
